c138ceeff82158ebb8bf80877e47621361dd55d4
Muhammad Abdullahi Jibrin <muhammadabdullahijibrin0@gmail.com>
2026-09-24 14:17:51 +0100

    Remove the admin creation from wsgi

--- backend/core/wsgi.py
+++ backend/core/wsgi.py
@@ -14,12 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
-
-# --- ADDED CODE TO AUTOMATICALLY CREATE THE SUPERUSER ---
-try:
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-    if not User.objects.filter(username='superadmin').exists():
-        User.objects.create_superuser('superadmin', 'admin@example.com', 'super1234')
-except Exception as e:
-    print(f"Superuser creation error: {e}")
